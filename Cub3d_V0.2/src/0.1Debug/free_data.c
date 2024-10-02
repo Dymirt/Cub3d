e6c791e1e689ec89b3236fad82f423b448a54320
mazen-tech <104716013+mazen-tech@users.noreply.github.com>
2024-10-02 16:04:09 +0200

    move version to dev_branch

---
+++ Cub3d_V0.2/src/0.1Debug/free_data.c
@@ -0,0 +1,50 @@
+#include "cub3d.h"
+
+void	free_2Darray(void **AD)
+{
+	size_t	i;
+
+	i = 0;
+	while (AD[i])
+	{
+		free(AD[i]);
+		i++;
+	}
+	if (AD)
+	{
+		free(AD);
+		AD = NULL;
+	}
+}
+
+static void	free_texinfo(t_texinfo *textures)
+{
+	if (textures->north)
+		free(textures->north);
+	if (textures->south)
+		free(textures->south);
+	if (textures->west)
+		free(textures->west);
+	if (textures->east)
+		free(textures->east);
+	if (textures->floor)
+		free(textures->floor);
+	if (textures->ceiling)
+		free(textures->ceiling);
+}
+
+int	all_free(t_data *data)
+{
+	if (data->map_info.fd > 0)
+		close(data->map_info.fd);
+	if (data->map_info.file)
+		free_2Darray((void **)data->map_info.file);
+	if (data->map)
+		free_2Darray((void **)data->map);
+	if (data->tex)
+		free_2Darray((void **)data->tex);
+	if (data->tex_pixels)
+		free_2Darray((void **)data->tex_pixels);
+	free_texinfo(&data->texinfo);
+	return (FAILURE);
+}
